0bb34f97e10f980b76301f4d2ad34f3dc72b6f16
matheusm25 <matheusml25@hotmail.com>
2022-12-26 09:25:22 -0300

    chore: testing github pages

---
+++ build/precache-manifest.8183d3bbd2ad389bcb3a04ee0d2f8997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4284c822edb707f28b14817ec7f625de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a648c8ca3b75575f3b2",
+    "url": "/static/js/2.a7b9aa40.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a7b9aa40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c4d7537bb5facb4d19",
+    "url": "/static/js/main.87beb80a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.87beb80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd62166a4ad241280130",
+    "url": "/static/js/runtime-main.75f641dd.js"
+  }
+]);
